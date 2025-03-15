a88bd90bc66aea658560e8948d1d55d7adb25d37
Felix G <76596162+felixgao-0@users.noreply.github.com>
2025-03-15 21:57:14 +0000

    feat: make modal css work

--- src/app/components/paymentModal.tsx
+++ src/app/components/paymentModal.tsx
@@ -1,7 +1,9 @@
 export default function PaymentModal() {
-    return (
-    <div>
+    return (<>
+    <div id="payment-modal" className={"hidden fixed inset-0 z-10 overflow-auto bg-black bg-opacity-40"}>
+    <div className={"bg-white my-16 mx-auto p-5 border border-gray-400 w-4/5 text-black font-mono"}>
         <h3 className={"text-2xl"}>Payment Required to Proceed</h3>
+        <span className={"text-gray-400 float-right text-2xl font-bold hover:text-black hover:no-underline cursor-pointer"}>&times;</span>
         <div className={"flex flex-col space-y-2"}>
             <div className={"flex flex-col"}>
                 <label>Credit Card Number</label>
@@ -28,5 +30,6 @@ export default function PaymentModal() {
             </div>
          </div>
     </div>
-    )
+    </div>
+    </>)
 }
